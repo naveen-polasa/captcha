201ed6c05aff1b1614ec65d26b6db0bd2a01f3eb
Naveen Polasa <naveenpolasa99@gmail.com>
2023-04-05 10:43:36 +0530

    update files

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,10 +1,44 @@
 import { useState } from "react";
 import { BsFillMoonFill } from "react-icons/bs";
-import Captcha from "./Captcha";
+import { TbReload } from "react-icons/tb";
+import { generateCaptcha } from "../utils/constants";
+
 export const SignUp = () => {
   const [theme, toggleTheme] = useState("black");
   const handleTheme = () =>
     theme === "black" ? toggleTheme("white") : toggleTheme("black");
+  const [captcha, changeCaptcha] = useState(generateCaptcha());
+  const [userVal, setUserVal] = useState("");
+  const [result, newResult] = useState(null);
+
+  const validate = () => {
+    if (!userVal) return;
+    newResult(
+      String(userVal) === captcha
+        ? { msg: "Validation Passed", res: true }
+        : { msg: "Validation Failed", res: false }
+    );
+    setUserVal("");
+    changeCaptcha(generateCaptcha());
+    const timeout = setTimeout(() => {
+      newResult(null);
+    }, 3000);
+    return () => clearTimeout(timeout);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    validate();
+    if (result?.res) return;
+    const data = new FormData(e.currentTarget);
+    const formData = Object.fromEntries(data);
+    const values = Object.values(formData);
+    if (values.includes("")) {
+      newResult({ msg: "All fields should be non empty", res: false });
+      return;
+    }
+    e.currentTarget.reset();
+    newResult({ msg: "Form Submission Successful", res: true });
+  };
   return (
     <div
       className={`flex items-center justify-center min-h-screen pb-20 ${
@@ -26,8 +60,8 @@ export const SignUp = () => {
             Sign Up Form with Captcha
           </h2>
           <form
-            className="mt-8 w-[80%] mx-auto  sm:w-[30rem]"
-            onSubmit={(e) => e.preventDefault()}
+            className="mt-8 w-[85vw] mx-auto  sm:w-[30rem]"
+            onSubmit={handleSubmit}
           >
             <div className="space-y-5">
               <div>
@@ -36,6 +70,7 @@ export const SignUp = () => {
                 </label>
                 <div className="mt-2.5">
                   <input
+                    name="fullname"
                     className="flex h-12 w-full rounded-md border border-gray-300 bg-transparent py-2 px-3 text-lg placeholder:text-gray-400 "
                     type="text"
                     placeholder="Enter You Full Name"
@@ -49,6 +84,7 @@ export const SignUp = () => {
                 </label>
                 <div className="mt-2.5">
                   <input
+                    name="email"
                     className="flex h-12 w-full rounded-md border border-gray-300 bg-transparent py-2 px-3 text-lg placeholder:text-gray-400 "
                     type="email"
                     placeholder="Enter Your Email"
@@ -62,6 +98,7 @@ export const SignUp = () => {
                 </label>
                 <div className="mt-2.5">
                   <input
+                    name="password"
                     className="flex h-12 w-full rounded-md border border-gray-300 bg-transparent py-2 px-3 text-lg placeholder:text-gray-400 "
                     type="password"
                     placeholder="Enter Your Password"
@@ -69,10 +106,39 @@ export const SignUp = () => {
                 </div>
               </div>
               <div>
-                <Captcha />
+                <div>
+                  <div className="flex gap-4 text-2xl items-center">
+                    <div className="h-12 w-40 font-semibold bg-red-300 flex items-center justify-center">
+                      <h3 className="underline decoration-wavy ">{captcha}</h3>
+                    </div>
+                    <button onClick={() => changeCaptcha(generateCaptcha())}>
+                      <TbReload size={32} />
+                    </button>
+                  </div>
+                  <div className="flex gap-x-2 items-center mt-2.5">
+                    <input
+                      name="captcha"
+                      className="flex h-12 w-40 sm:w-52 rounded-md border border-gray-300 bg-transparent py-2 px-3 text-lg placeholder:text-gray-400 "
+                      type="text"
+                      placeholder="Enter Captcha"
+                      value={userVal}
+                      onChange={(e) => setUserVal(e.target.value)}
+                    ></input>
+                  </div>
+                  <p
+                    className={`${
+                      result?.res ? "bg-green-400" : "bg-red-400"
+                    } px-1 rounded-md break-words w-24 sm:w-full text-center my-2`}
+                  >
+                    {result?.msg}
+                  </p>
+                </div>
               </div>
               <div>
-                <button className="w-full inline-flex items-center justify-center rounded-md bg-indigo-600 px-3.5 py-2.5 font-semibold  text-white hover:bg-indigo-500">
+                <button
+                  type="submit"
+                  className="w-full inline-flex items-center justify-center rounded-md bg-indigo-600 px-3.5 py-2.5 font-semibold  text-white hover:bg-indigo-500"
+                >
                   Sign Up
                 </button>
               </div>
--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -5,16 +5,14 @@ const generateNumber = (st, en) => {
 
 export const generateCaptcha = () => {
   let captcha = "";
-  for (let i = 0; i < 9; i++) {
-    if (i >= 3 && i < 6) {
-      captcha += String(generateNumber(-1, 9));
-    }
-    if (i >= 0 && i < 3) {
-      captcha += String.fromCharCode(generateNumber(96, 122));
-    }
-    if (i >= 6 && i < 9) {
-      captcha += String.fromCharCode(generateNumber(66, 90));
-    }
+  for (let i = 0; i < 7; i++) {
+    const data = [
+      String(generateNumber(-1, 9)),
+      String.fromCharCode(generateNumber(96, 122)),
+      String.fromCharCode(generateNumber(66, 90)),
+    ];
+    const op = generateNumber(-1, 2);
+    captcha += data[op];
   }
   return captcha;
 };
